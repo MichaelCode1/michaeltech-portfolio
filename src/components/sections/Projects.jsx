3b803070981a32e0e2fd3b34f141cc4f9cf75dd4
Michael <mppattis@ualberta.ca>
2025-07-08 14:08:15 -0600

    Edel Added

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,9 +14,65 @@ export const Projects = () => {
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
                             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                <h3 className="text-xl font-bold mb-2"> Continuous-Thrust Orbit Transfer</h3>
+                                <p className="text-gray-400 mb-4"> 
+                                    Simulated and visualized near-optimal continuous-thrust orbit transfer without
+                                    approximations.
+                                </p>
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {["Python","Orbital Analysis","NumPy", "SciPy"].map((tech,key) => (
+                                        <span 
+                                            key={key}
+                                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                                        hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition
+                                        ">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+
+                                <div className="flex justify-between items-center">
+                                    <a 
+                                    href="https://github.com/MichaelCode1/Continuous-Thrust-Orbit-Transfer" 
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                    > 
+                                        In Progress →  
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                <h3 className="text-xl font-bold mb-2"> Low-Thrust Orbit Transfer</h3>
+                                <p className="text-gray-400 mb-4"> 
+                                    Simulated and visualized low-thrust orbit transfer (300 km to 550 km) with the Edelbaum
+                                    Approximation.
+                                </p>
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {["Python","Orbital Analysis","NumPy", "SciPy"].map((tech,key) => (
+                                        <span 
+                                            key={key}
+                                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                                        hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition
+                                        ">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+
+                                <div className="flex justify-between items-center">
+                                    <a 
+                                    href="https://github.com/MichaelCode1/Edelbaum-Approximation" 
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                    > 
+                                        View Project →  
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                                 <h3 className="text-xl font-bold mb-2"> Hohmann Transfer (LEO to GEO)</h3>
                                 <p className="text-gray-400 mb-4"> 
-                                    Simulated and visualized a Hohmann transfer 
+                                    Simulated and visualized Hohmann transfer 
                                     from LEO to GEO with user-defined rocket parameters. Example shown using SpaceX Starship.
                                 </p>
                                 <div className="flex flex-wrap gap-2 mb-4">
